faf702b5ad6348bf54668db5f8ea0ea1e79f0b80
tomszas <tomszpit@wp.pl>
2017-06-03 10:26:23 +0200

    Two controllers have been added.

---
+++ project1/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace warsztaty.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get() => Content("Hello from warsztaty api :)");
+    }
+}
---
+++ project1/Controllers/RecordsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using warsztaty.messages.Commands;
+
+namespace warsztaty.API.Controllers
+{
+    [Route("[controller]")]
+    public class RecordsController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]CreateRecord command)
+        {
+            return Accepted(); // 202
+        }
+    }
+}
